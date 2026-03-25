0b2bb5cdff90503bffcfc44834f58f060c511bbd
Md. Aslam Hossain <hossain.aslam.dev@gmail.com>
2026-03-25 06:00:54 +0000

    dataType summery

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,33 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outdsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId );
+
+// const bigNumber = 73408752087456329n
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions,
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let = myObj = {
+  name: "Aslam",
+  age: 22,
+};
+
+const myFunction = function () {
+  console.log("Hello world");
+};
+
+console.log(typeof heros);
