692421193859b5ac23ed16b6be352245a3cd3b8c
Md. Aslam Hossain <hossain.aslam.dev@gmail.com>
2026-03-25 00:00:51 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 1445533;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+// accountId = 2; // not allowed
+
+accountEmail = "hec@hc.com"
+accountPassword = "7295629"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
