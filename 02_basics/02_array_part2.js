58db89063879d2feca05e05a96735924dc6fc93c
Md. Aslam Hossain <hossain.aslam.dev@gmail.com>
2026-03-26 05:29:20 +0000

    Array part 2

---
+++ 02_basics/02_array_part2.js
@@ -0,0 +1,35 @@
+const marverl_heros = ["Thor", "Ironman", "Spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marverl_heros.push(dc_heros)
+
+// console.log(marverl_heros);
+// console.log(marverl_heros[3][1]);
+
+
+// const allHeros = marverl_heros.concat(dc_heros)
+// console.log(allHeros);
+
+// const all_new_heros = [...marverl_heros, ...dc_heros]  // This is the most preferable than the concat by the industrial people  // This also called Spread Operator
+
+// console.log(all_new_heros);
+
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]] ]
+
+const real_another_array = another_array.flat(Infinity)
+
+// console.log(real_another_array);
+
+
+
+//  console.log(Array.isArray("Aslam"))
+//  console.log(Array.from("Aslam"))
+//  console.log(Array.from({name: "Aslam"})) // interesting case for interview //from with include name:Aslam
+
+
+ let score1 = 100
+ let score2 = 200
+ let score3 = 300
+
+ console.log(Array.of(score1, score2, score3));
