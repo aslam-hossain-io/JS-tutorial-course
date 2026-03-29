7713a279ed9c1fddbcba6932520446b42dc5abed
Md. Aslam Hossain <hossain.aslam.dev@gmail.com>
2026-03-29 06:54:31 +0000

    scope(Global and local

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,22 @@
+// var c = 300;
+let a = 300
+
+if (true) {
+  let a = 10;
+  const b = 20;
+  var c = 30;
+  console.log("INNER: ", a);
+  function addnum(){
+    addnum = 1
+    return;
+  }
+  
+}
+
+for (let i=0; i< Array.length; i++){
+  const element = array[i];
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
